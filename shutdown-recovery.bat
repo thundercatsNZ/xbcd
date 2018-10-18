cfa76502913c5297186551d8ebe66cd47eac7f87
Paul <25402358+thundercatsNZ@users.noreply.github.com>
2018-10-18 17:10:05 +0100

    Legacy install files

---
+++ shutdown-recovery.bat
@@ -0,0 +1,36 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+echo Manual signature enforcement disable system.
+echo  
+echo This will shut down your system into recovery mode.
+echo 1. Go into troubleshoot.
+echo 2. Go into advanced options.
+echo 3. Go into Start-Up settings.
+echo 4. Select option 7 and your PC will reboot with signature enforcement disabled.
+echo Good Luck!
+echo  
+pause
+
+shutdown.exe /r /o
